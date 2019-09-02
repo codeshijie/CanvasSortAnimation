80af5304617587e72e2e6d8f79b6be521d01dfa7
xingyilong <xingyilong@aikucun.com>
2019-09-02 17:32:50 +0800

    排序延时

--- src/js/modules/ShellSort.js
+++ src/js/modules/ShellSort.js
@@ -1,21 +1,24 @@
 export default class ShellSort {
     constructor(canvas) {
         this.sortName = '希尔排序算法';
-        this.elementCount = 10;
+        this.elementCount = 16;
         this.array = null;
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
         this.animationDuration = 400;
         this.xCoordFactor = this.canvas.width / this.elementCount;
         this.yCoordFactor = 40;
-        this.fontSize = 14;
+        this.fontSize = 12;
         this.ctx.font = ` ${this.fontSize}px serif`;
+
+        this.sortNowDate = {};
     }
     begin() {
         this.random();
         console.log(this.array);
-        this.sort(Math.floor(this.array.length / 2));
         this.draw();
+        this.sort(Math.floor(this.array.length / 2));
+
 
     }
     random() {
@@ -27,7 +30,12 @@ export default class ShellSort {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (let i = 0; i < this.array.length; i++) {
-            this.drawOne(this.xCoordFactor * (i), this.yCoordFactor, this.array[i])
+            console.log(this.sortNowDate.begin, this.sortNowDate.group)
+            if (this.sortNowDate.begin != undefined && i % this.sortNowDate.group === this.sortNowDate.begin) {
+                this.drawOne(this.xCoordFactor * (i), this.yCoordFactor + this.xCoordFactor, this.array[i])
+            } else {
+                this.drawOne(this.xCoordFactor * (i), this.yCoordFactor, this.array[i])
+            }
         }
     }
     drawOne(x, y, value) {
@@ -43,12 +51,10 @@ export default class ShellSort {
     }
     sort(group, begin = 0) {
         this.sortInsert(begin, group).then(res => {
-            if (begin < group) {
+            if (begin + 1 < group) {
                 this.sort(group, ++begin);
             } else if (group > 1) {
                 this.sort(Math.floor(group / 2));
-            } else if (group == 1) {
-                this.draw();
             }
         })
     }
@@ -57,9 +63,15 @@ export default class ShellSort {
         let promise = new Promise((res) => {
             resolve = res;
         });
+        this.sortNowDate = {
+            begin,
+            group
+        }
+        this.draw();
         this.sortInsertOne(begin, group, resolve)
         return promise;
     }
+
     sortInsertOne(begin, group, resolve) {
         this.sortInsertOneCompare(begin, group).then(() => {
             if (begin < this.array.length) {
@@ -77,7 +89,10 @@ export default class ShellSort {
         if (this.array[i] < this.array[i - group]) {
             this.sortInsertOneMove(i, group, this.array[i], resolve);
         } else {
-            resolve();
+            setTimeout(() => {
+                resolve();
+            }, this.animationDuration)
+
         }
         return promise;
     }
@@ -87,10 +102,14 @@ export default class ShellSort {
             setTimeout(() => {
                 this.array[j + group] = this.array[j];
                 this.sortInsertOneMove(j, group, moveValue, resolve);
-            }, 100)
+            }, this.animationDuration)
         } else {
-            this.array[j + group] = moveValue;
-            resolve();
+            setTimeout(() => {
+                this.array[j + group] = moveValue;
+                resolve();
+            }, this.animationDuration)
+
         }
+        console.log(this.array);
     }
 }
