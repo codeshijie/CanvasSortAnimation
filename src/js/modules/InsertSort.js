48c217c792ddb58f3c3f25749e64b2dd7ec68b2f
xingyilong <xingyilong@aikucun.com>
2019-09-03 13:09:45 +0800

    y time

--- src/js/modules/InsertSort.js
+++ src/js/modules/InsertSort.js
@@ -3,5 +3,6 @@ export default class InsertSort extends ShellSort {
     constructor(canvas, elementCount = 16,fontSize = 13) {
         super(canvas, elementCount,fontSize);
         this.yCoordFactor = this.yCoordFactor * 6;
+        this.yMoveDuration = 0;
     }
 }
